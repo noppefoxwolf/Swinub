33ef5f654da70bd56798d4262d17cc3598842b60
noppefoxwolf <noppelabs@gmail.com>
2023-12-11 03:05:22 +0900

    Update GetV1AccountsRelationships.swift

--- Sources/Swinub/V1/Accounts/GetV1AccountsRelationships.swift
+++ Sources/Swinub/V1/Accounts/GetV1AccountsRelationships.swift
@@ -4,18 +4,18 @@ import HTTPTypes
 public struct GetV1AccountsRelationships: AuthorizationRequest {
     public typealias Response = [Relationship]
 
-    public init(ids: [String], authorization: Authorization) {
+    public init(ids: [Account.ID], authorization: Authorization) {
         self.accountIDs = ids
         self.authorization = authorization
     }
 
-    public var accountIDs: [String]
+    public var accountIDs: [Account.ID]
     public var byAccountID: Account.ID { authorization.accountID }
     public let authorization: Authorization
     public var path: String { "/api/v1/accounts/relationships" }
     public let method: HTTPRequest.Method = .get
     public var authority: String { authorization.host }
     public var parameters: [String : (any RequestParameterValue)?] {
-        ["id": accountIDs.joined(separator: ",")]
+        ["id": accountIDs.map(\.rawValue).joined(separator: ",")]
     }
 }

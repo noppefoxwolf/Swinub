8b51979757f8fecafbcb013d7a7c5a4812a1fd6c
noppefoxwolf <noppelabs@gmail.com>
2024-01-14 15:12:35 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.8
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "Swinub",
-    platforms: [.iOS(.v16), .macOS(.v14)],
+    platforms: [.iOS(.v16), .macOS(.v13)],
     products: [
         .library(
             name: "Swinub",

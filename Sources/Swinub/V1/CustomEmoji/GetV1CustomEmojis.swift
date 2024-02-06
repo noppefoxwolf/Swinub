031d6bc075c20fdefa0f1482efe4457ab4e33d04
noppefoxwolf <noppelabs@gmail.com>
2024-02-06 20:26:46 +0900

    Update GetV1CustomEmojis.swift

--- Sources/Swinub/V1/CustomEmoji/GetV1CustomEmojis.swift
+++ Sources/Swinub/V1/CustomEmoji/GetV1CustomEmojis.swift
@@ -1,13 +1,14 @@
 import Foundation
 import HTTPTypes
 
-public struct GetV1CustomEmojis: Request, Sendable {
+public struct GetV1CustomEmojis: OptionalAuthorizationRequest, Sendable {
     public typealias Response = [CustomEmoji]
 
     public init(host: String) {
         self.host = host
     }
 
+    public var authorization: Authorization? = nil
     public let host: String
     public var authority: String { host }
     public var path: String { "/api/v1/custom_emojis" }

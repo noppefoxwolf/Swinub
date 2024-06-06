752c469a3e1aa3b44e3bc7ddfac4a1bb4ed03b07
noppefoxwolf <noppelabs@gmail.com>
2024-06-07 05:23:01 +0900

    Update EndpointRequest.swift

--- Sources/Swinub/Request/EndpointRequest.swift
+++ Sources/Swinub/Request/EndpointRequest.swift
@@ -4,10 +4,19 @@ import Foundation
 public protocol EndpointRequest: Sendable {
     associatedtype Response: Decodable & Sendable
     associatedtype AuthorizationType: Sendable
+    
+    /// https
     var scheme: String { get }
+    
+    /// example.com
     var authority: String { get }
+    
+    /// /users/1
     var path: String { get }
+    
+    /// https://example.com/users/1
     var url: URL { get throws }
+    
     var authorization: AuthorizationType { get }
     
     var parameters: [String: (any RequestParameterValue)?] { get }

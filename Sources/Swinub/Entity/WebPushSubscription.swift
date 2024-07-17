976161ebe6f28be2f3bbdbd96cc30790577deb9e
noppe <noppelabs@gmail.com>
2024-07-18 00:02:03 +0900

    supports String ID type

--- Sources/Swinub/Entity/WebPushSubscription.swift
+++ Sources/Swinub/Entity/WebPushSubscription.swift
@@ -7,8 +7,26 @@ public struct WebPushSubscription: Codable, Sendable {
         public let poll: Bool?
         public let emojiReaction: Bool?
     }
+    
+    public enum ID: Codable, Sendable {
+        case int(Int)
+        case string(String)
+        
+        public init(from decoder: any Decoder) throws {
+            let container = try decoder.singleValueContainer()
+            do {
+                let intValue = try container.decode(Int.self)
+                self = .int(intValue)
+            } catch is Swift.DecodingError {
+                let stringValue = try container.decode(String.self)
+                self = .string(stringValue)
+            } catch {
+                throw error
+            }
+        }
+    }
 
-    public let id: String
+    public let id: ID
     public let endpoint: String
     public let alerts: Alerts?
     public let serverKey: String
---
+++ Tests/SwinubTests/WebPushSubscriptionTests.swift
@@ -0,0 +1,35 @@
+import Swinub
+import XCTest
+
+class WebPushSubscriptionTests: XCTestCase {
+    func testDecodeStringID() async throws {
+        let json = """
+        {
+            "id": "your_id_here",
+            "endpoint": "your_endpoint_here",
+            "alerts": null,
+            "serverKey": "your_server_key_here"
+        }
+        """
+        let data = Data(json.utf8)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        _ = try decoder.decode(WebPushSubscription.self, from: data)
+    }
+    
+    func testDecodeIntID() async throws {
+        let json = """
+        {
+            "id": 999,
+            "endpoint": "your_endpoint_here",
+            "alerts": null,
+            "serverKey": "your_server_key_here"
+        }
+        """
+        let data = Data(json.utf8)
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        _ = try decoder.decode(WebPushSubscription.self, from: data)
+    }
+}
+

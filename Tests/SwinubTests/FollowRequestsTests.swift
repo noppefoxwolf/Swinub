42c7f8bdac9895d9445092ce4b97ef7a2fada61e
noppe <noppelabs@gmail.com>
2025-07-07 20:49:47 +0900

    move files

--- Tests/SwinubTests/FollowRequestsTests.swift
+++ Tests/SwinubTests/FollowRequestsTests.swift
@@ -4,16 +4,4 @@ import HTTPTypes
 import Foundation
 
 @Suite
-struct FollowRequestsTests {
-    @Test
-    func getFollowRequests() async throws {
-        let authorization = Authorization(
-            host: "",
-            accountID: Account.ID(rawValue: ""),
-            oauthToken: ""
-        )
-        let request = GetV1FollowRequests(authorization: authorization)
-        let response = try await SwinubDefaults.session.response(for: request)
-        print(response)
-    }
-}
+struct FollowRequestsTests {}

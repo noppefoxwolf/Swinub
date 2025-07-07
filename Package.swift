d680e27db960357d3b0b2c5fdfd622291fab6b0c
noppe <noppelabs@gmail.com>
2025-07-07 23:42:32 +0900

    update

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.0"),
-        .package(url: "https://github.com/apple/swift-http-types", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-async-algorithms", from: "1.0.4"),
+        .package(url: "https://github.com/apple/swift-http-types", from: "1.4.0"),
     ],
     targets: [
         .target(

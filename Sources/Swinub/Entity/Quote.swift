a57fe1228c36ba657c84e633263f8f1f1aba8833
noppe <noppelabs@gmail.com>
2025-07-08 18:46:44 +0900

    Update Quote.swift

--- Sources/Swinub/Entity/Quote.swift
+++ Sources/Swinub/Entity/Quote.swift
@@ -11,28 +11,28 @@ public enum QuoteState: String, CaseIterable, Codable, Sendable {
 
 public struct Quote: Codable, Sendable {
     public let state: QuoteState
-    public let status: Indirect<Status>?
+    public let quotedStatus: Indirect<Status>?
     
     public init(from decoder: any Decoder) throws {
         var state: QuoteState
-        var status: Indirect<Status>?
+        var quotedStatus: Indirect<Status>?
         
         do {
             let container = try decoder.container(keyedBy: CodingKeys.self)
             state = try container.decode(QuoteState.self, forKey: .state)
-            status = try container.decodeIfPresent(Indirect<Status>.self, forKey: .status)
+            quotedStatus = try container.decodeIfPresent(Indirect<Status>.self, forKey: .quotedStatus)
         } catch {
             do {
                 // fedibirdの場合
                 let container = try decoder.singleValueContainer()
                 state = .accepted
-                status = try container.decode(Indirect<Status>.self)
+                quotedStatus = try container.decode(Indirect<Status>.self)
             } catch {
                 throw error
             }
         }
         
         self.state = state
-        self.status = status
+        self.quotedStatus = quotedStatus
     }
 }

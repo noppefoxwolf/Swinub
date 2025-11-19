386baffee0f556420011fb8cf663f477fc179245
noppe <noppelabs@gmail.com>
2025-11-19 23:29:05 +0900

    Update Preference.swift

--- Sources/Swinub/Entity/Preference.swift
+++ Sources/Swinub/Entity/Preference.swift
@@ -3,6 +3,7 @@ public struct Preference: Codable, Sendable {
         case postingDefaultVisibility = "posting:default:visibility"
         case postingDefaultSensitive = "posting:default:sensitive"
         case postingDefaultLanguage = "posting:default:language"
+        case postingDefaultQuotePolicy = "posting:default:quote_policy"
         case readingExpandMedia = "reading:expand:media"
         case readingExpandSpoilers = "reading:expand:spoilers"
         case readingAutoplayGifs = "reading:autoplay:gifs"
@@ -10,6 +11,7 @@ public struct Preference: Codable, Sendable {
     public let postingDefaultVisibility: StatusVisibility?
     public let postingDefaultSensitive: Bool?
     public let postingDefaultLanguage: String?
+    public let postingDefaultQuotePolicy: QuoteApprovalPolicy?
     public let readingExpandMedia: String?
     public let readingExpandSpoilers: Bool?
     public let readingAutoplayGifs: Bool?

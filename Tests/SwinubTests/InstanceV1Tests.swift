0ce4aff9671c46679948d75908e4f6d9e1f8a41f
noppe <noppelabs@gmail.com>
2026-09-04 20:08:41 +0900

    Add API version selection to instance CLI

--- Tests/SwinubTests/InstanceV1Tests.swift
+++ Tests/SwinubTests/InstanceV1Tests.swift
@@ -50,4 +50,53 @@ struct InstanceV1Tests {
 
         #expect(instance.featureQuote == true)
     }
+
+    @Test func decodesSharkeyCompatibleResponseWithoutShortDescription() throws {
+        let json = """
+            {
+              "uri": "ddoskey.com",
+              "title": "Sharkey",
+              "description": "",
+              "version": "3.0.0",
+              "stats": {
+                "user_count": 1,
+                "status_count": 2,
+                "domain_count": 3
+              },
+              "configuration": {
+                "accounts": {
+                  "max_featured_tags": 20
+                },
+                "statuses": {
+                  "max_characters": 3000,
+                  "max_media_attachments": 16
+                },
+                "media_attachments": {
+                  "supported_mime_types": [],
+                  "image_size_limit": 1,
+                  "image_matrix_limit": 2,
+                  "video_size_limit": 3,
+                  "video_frame_limit": 4,
+                  "video_matrix_limit": 5
+                },
+                "polls": {
+                  "max_options": 10,
+                  "max_characters_per_option": 150,
+                  "min_expiration": 50,
+                  "max_expiration": 2629746
+                }
+              },
+              "registrations": true,
+              "approval_required": false
+            }
+            """
+
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        let instance = try decoder.decode(InstanceV1.self, from: Data(json.utf8))
+
+        #expect(instance.shortDescription == nil)
+        #expect(instance.registrations == true)
+        #expect(instance.approvalRequired == false)
+    }
 }
